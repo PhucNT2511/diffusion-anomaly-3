1b1ba3b5a373fd6092e3641f348dc67f3f24102a
Nguyen Thang Phuc <phuca1tt1bn@gmail.com>
2024-12-26 17:56:16 +0700

    saved

--- scripts/classifier_train.py
+++ scripts/classifier_train.py
@@ -364,7 +364,7 @@ def create_argparser():
         data_dir="",
         val_data_dir="",
         noised=True,
-        iterations=200001,
+        iterations=400001,
         lr=3e-4, ########## Tăng lr để nhảy xuống cực trị nhanh ở thời điểm ban đầu, giảm dần ở steps sau
         weight_decay=0.0,
         anneal_lr=True,
