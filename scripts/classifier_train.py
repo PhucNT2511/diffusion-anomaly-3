f5008eeed5d60fc29dd0b51a8a47640ec111d31c
Nguyen Thang Phuc <phuca1tt1bn@gmail.com>
2025-02-05 09:46:12 +0700

    saved

--- scripts/classifier_train.py
+++ scripts/classifier_train.py
@@ -384,14 +384,14 @@ def create_argparser():
         data_dir="",
         val_data_dir="",
         noised=True,
-        iterations=100001,
+        iterations=200001,
         lr=3e-4, ########## 1e-3-SGD / 3e-4 ADAMW - Tăng lr để nhảy xuống cực trị nhanh ở thời điểm ban đầu, giảm dần ở steps sau
         weight_decay=0.0, ######### 5e-4-SGD; 0.0 ADAMW 
         anneal_lr=True,
         batch_size=32,
         microbatch=-1,
         schedule_sampler="uniform",
-        resume_checkpoint="/kaggle/working/diffusion-anomaly-3/checkpoint/classifier/model050000.pt",
+        resume_checkpoint="/kaggle/working/diffusion-anomaly-3/checkpoint/classifier/model100000.pt",
         log_interval=10,
         eval_interval=1000, # sau 1000 steps sẽ in ra kết quả evaluate
         save_interval=10000, # sau 10000 steps sẽ lưu lại một lần
