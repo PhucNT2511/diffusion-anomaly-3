11fc89174b8b38e5eef588c474ab8697cd39c879
Nguyen Thang Phuc <phuca1tt1bn@gmail.com>
2025-02-07 04:06:26 +0700

    saved

--- scripts/image_train.py
+++ scripts/image_train.py
@@ -70,7 +70,7 @@ def main():
                     fold=args.fold, 
                     transforms=None,
                     only_positive = False,
-                    only_negative = True)
+                    only_negative = False)
 
 
     loader = DataLoader(
