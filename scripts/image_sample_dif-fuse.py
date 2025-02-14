88831c19f210ad6ae9f7c49d12ef968166cfa2e6
Nguyen Thang Phuc <phuca1tt1bn@gmail.com>
2025-02-14 17:11:58 +0700

    saved

--- scripts/image_sample_dif-fuse.py
+++ scripts/image_sample_dif-fuse.py
@@ -201,8 +201,8 @@ def create_argparser():
         model_path="", ############# các path này phải lấy kỹ, theo fold
         classifier_path="", ################ path này lấy kỹ, theo fold
         fold = 1,
-        start_point = 500,
-        end_point = 1000,
+        start_point = 1000,
+        end_point = 1500,
     )
     defaults.update(model_and_diffusion_defaults())
     defaults.update(classifier_defaults())
