11161b9c18c583d5a8dc1686602a8fb414676d3e
Nguyen Thang Phuc <phuca1tt1bn@gmail.com>
2025-02-14 15:51:51 +0700

    saved

--- scripts/image_sample_dif-fuse.py
+++ scripts/image_sample_dif-fuse.py
@@ -147,14 +147,17 @@ def main():
 
             
             sample_img = sample.to(torch.float)
-    
+
+            out_path = os.path.join(logger.get_dir(), 'images')
+            if not os.path.exists(out_path):
+                os.makedirs(out_path)
+
             for j in range(sample_img.shape[0]):
                 out_path_img_sample = os.path.join(logger.get_dir(),
                                                     f"images/batch{i}_noiselevel_{noise_level}_threshold_{threshold}_kernelsize_{kernel}_ind_{independent}_{ids[j][40:-4]}_sample.npy")
                 with open(out_path_img_sample, 'wb') as f:
                     np.save(f, np.array(sample_img[j, :, :, :]))
                 print(f'Process {max(i-1,0)*args.batch_size+j+1} images completely!')
-            
             '''
             fig = plt.figure(figsize=(11,11))
             for j in range(args.batch_size):
@@ -198,8 +201,8 @@ def create_argparser():
         model_path="", ############# các path này phải lấy kỹ, theo fold
         classifier_path="", ################ path này lấy kỹ, theo fold
         fold = 1,
-        start_point = 1000,
-        end_point = 1500,
+        start_point = 0,
+        end_point = 500,
     )
     defaults.update(model_and_diffusion_defaults())
     defaults.update(classifier_defaults())
