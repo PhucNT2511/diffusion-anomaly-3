c7478854489e50b9ed1afd1105f6f3e05015d9d3
Nguyen Thang Phuc <phuca1tt1bn@gmail.com>
2025-03-14 16:59:00 +0700

    saved

--- scripts/classifier_train.py
+++ scripts/classifier_train.py
@@ -413,7 +413,7 @@ def create_argparser():
         batch_size=32,
         microbatch=-1,
         schedule_sampler="uniform",
-        resume_checkpoint=f"/kaggle/input/brats20-models-fold2/modelcls020000.pt",
+        resume_checkpoint="",#f"/kaggle/input/brats20-models-fold2/modelcls020000.pt",
         log_interval=10,
         eval_interval=1000,
         save_interval=10000,
