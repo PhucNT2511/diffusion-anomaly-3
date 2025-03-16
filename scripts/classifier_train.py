01b080cd845ce33abff7213adad43acbde4c76a3
Nguyen Thang Phuc <phuca1tt1bn@gmail.com>
2025-03-16 23:33:04 +0700

    saved

--- scripts/classifier_train.py
+++ scripts/classifier_train.py
@@ -260,15 +260,15 @@ def main():
             #
             logits = model(sub_batch, timesteps=sub_t)         
             loss_cls = F.cross_entropy(logits, sub_labels, reduction="none")
-
+            
             # Tính diversity loss trên các tầng Conv2d đã chọn:
             loss_div = 0.0
-            
+            '''
             for lname, layer_module in layers_to_finetune[:1]:
                 if isinstance(layer_module, nn.Conv2d):
                     loss_div = loss_div + diversity_loss(layer_module)
             # Tổng loss: kết hợp loss phân loại và diversity loss
-            
+            '''
             loss = loss_cls + lambda_div * loss_div
 
             losses = {}
