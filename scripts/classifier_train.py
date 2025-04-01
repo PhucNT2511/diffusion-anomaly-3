e085298435d5ef9a07d6a39d803eea440501859e
Nguyen Thang Phuc <phuca1tt1bn@gmail.com>
2025-04-01 21:36:16 +0700

    saved

--- scripts/classifier_train.py
+++ scripts/classifier_train.py
@@ -286,6 +286,7 @@ def main():
                 loss_centralization = th.norm((a - mean_a), p=2, dim=(1, 2, 3))
 
             print(f"loss_cls {loss_cls} - loss_centralization {loss_centralization}")
+            print(f"loss_cls.requires_grad: {loss_cls.requires_grad} - loss_centralization.requires_grad: {loss_centralization.requires_grad}" )
             # Tổng loss: kết hợp loss phân loại và diversity loss
             loss = loss_cls + 100 * loss_centralization
 
