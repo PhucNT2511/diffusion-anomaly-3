b9ae1404a0647e20a05004a950574d228c73cbe4
Nguyen Thang Phuc <phuca1tt1bn@gmail.com>
2025-04-02 01:25:46 +0700

    saved

--- guided_diffusion/gaussian_diffusion.py
+++ guided_diffusion/gaussian_diffusion.py
@@ -489,7 +489,7 @@ class GaussianDiffusion:
                     low=0, high=2, size=(x.shape[0],), device=x.device
                 )
 
-                lambda_eff = 1
+                lambda_eff = 100
 
                 for _ in range(20 - 1):
                     optimizer.zero_grad()
