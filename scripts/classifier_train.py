bbdb05a5ac9dec080eaae5d57beb325f07d38754
Nguyen Thang Phuc <phuca1tt1bn@gmail.com>
2025-04-01 18:04:05 +0700

    saved

--- scripts/classifier_train.py
+++ scripts/classifier_train.py
@@ -283,7 +283,7 @@ def main():
             a=th.autograd.grad(selected.sum(), sub_batch_0)[0]
             mean_a = th.mean(a, dim=(2, 3), keepdim=True)
             loss_centralization = th.norm((a - mean_a), p=2, dim=(1, 2, 3))
-            print(f"loss_cls {loss_cls.shape} - loss_centralization {loss_centralization.shape}")
+            print(f"loss_cls {loss_cls} - loss_centralization {loss_centralization}")
             # Tổng loss: kết hợp loss phân loại và diversity loss
             loss = loss_cls + 100 * loss_centralization
 
