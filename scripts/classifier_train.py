6560820344c0acf7734f55e24caff4be33cbcd35
Nguyen Thang Phuc <phuca1tt1bn@gmail.com>
2025-04-02 17:44:08 +0700

    saved

--- scripts/classifier_train.py
+++ scripts/classifier_train.py
@@ -248,7 +248,7 @@ def main():
         batch_0 = batch
         if args.noised:
             t, _ = schedule_sampler.sample(batch.shape[0], dist_util.dev())
-            max_L_minus_t_square = ((1000 - t) ** 2).to(t.dtype).to(t.device)
+            #max_L_minus_t_square = ((1000 - t) ** 2).to(t.dtype).to(t.device)
             # print(f"{prefix}: batch_shape: {batch.shape} - noise_levels: {t}") ### max_L = 1000
             batch = diffusion.q_sample(batch, t)
         else:
@@ -260,8 +260,8 @@ def main():
         t_0 = th.zeros(batch_0.shape[0], dtype=th.long, device=dist_util.dev())
 
         ############################################################### Loss
-        for i, (sub_batch_0, sub_batch, sub_labels, sub_t, sub_t_0, sub_classes, sub_max_L_minus_t_square) in enumerate(
-            split_microbatches(args.microbatch, batch_0, batch, labels, t, t_0, classes, max_L_minus_t_square)
+        for i, (sub_batch_0, sub_batch, sub_labels, sub_t, sub_t_0, sub_classes) in enumerate(
+            split_microbatches(args.microbatch, batch_0, batch, labels, t, t_0, classes)
         ):
             #
             logits = model(sub_batch, timesteps=sub_t)         
@@ -293,7 +293,7 @@ def main():
             # Tổng loss: kết hợp loss phân loại và diversity loss
 
              
-            loss = loss_cls + 100*loss_centralization #* sub_max_L_minus_t_square  ### Sẽ chú ý phân loại đúng những cái ở đầu hơn
+            loss = loss_cls + 100 * loss_centralization  #* sub_max_L_minus_t_square  ### Sẽ chú ý phân loại đúng những cái ở đầu hơn
 
             losses = {}
             losses[f"{prefix}_loss"] = loss.detach()
@@ -438,7 +438,7 @@ def create_argparser():
         iterations= 100001, # must be more than step from checkpoint
         lr=1e-4,
         weight_decay=0.0,
-        anneal_lr=True,
+        anneal_lr=False,
         batch_size=4,
         microbatch=-1,
         schedule_sampler="uniform",
