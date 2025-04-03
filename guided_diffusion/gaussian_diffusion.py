67be2c8c39443735b8f01b999a55b88774e0961e
Nguyen Thang Phuc <phuca1tt1bn@gmail.com>
2025-04-03 23:28:52 +0700

    saved

--- guided_diffusion/gaussian_diffusion.py
+++ guided_diffusion/gaussian_diffusion.py
@@ -482,15 +482,10 @@ class GaussianDiffusion:
                 ##### Cách này đang muốn sự thay đổi nhỏ cho cfn trong khi vẫn dự đoán chính xác hơn
                 # Giả sử cfn ban đầu không thay đổi, ta chỉ học delta (điều chỉnh)
                 delta_cfn = th.zeros_like(cfn, requires_grad=True)
-                optimizer = th.optim.Adam([delta_cfn], lr=0.1)
+                optimizer = th.optim.AdamW([delta_cfn], lr=0.1)
 
                 labels = th.randint(low=0, high=1, size=(x.shape[0],), device=x.device)
                 lambda_eff = 1e6  # Hệ số regularization cho delta
-
-                for name, param in classifier.named_parameters():
-                    if param.requires_grad:
-                        print(name)
-
 
                 for _ in range(20):
                     print('delta_cfn: ',delta_cfn.unique())
