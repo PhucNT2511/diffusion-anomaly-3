fb12ed2900dd754f386765ae3097951d0ed52641
Nguyen Thang Phuc <phuca1tt1bn@gmail.com>
2025-04-14 11:10:20 +0700

    saved

--- guided_diffusion/gaussian_diffusion.py
+++ guided_diffusion/gaussian_diffusion.py
@@ -470,11 +470,11 @@ class GaussianDiffusion:
             else:
                 a, cfn = cond_fn(x, self._scale_timesteps(t).long(), **model_kwargs)
 
-            with th.enable_grad():
-
-                # Tính baseline logits từ mean ban đầu đã có cfn
+            # Tính baseline logits từ mean ban đầu đã có cfn
                 mean_baseline = out["mean"] + out["variance"] * cfn
                 logits_baseline = classifier(mean_baseline, timesteps=t-1)
+
+            with th.enable_grad():
 
                 # Khởi tạo delta_cfn với giá trị 0 và yêu cầu tính grad
                 delta_cfn = th.zeros_like(cfn, requires_grad=True)
