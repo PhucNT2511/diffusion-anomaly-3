eef67fbb5dd7466959fbe9a889fa618e7b41e784
Nguyen Thang Phuc <phuca1tt1bn@gmail.com>
2025-04-15 21:53:06 +0700

    saved

--- guided_diffusion/gaussian_diffusion.py
+++ guided_diffusion/gaussian_diffusion.py
@@ -473,6 +473,7 @@ class GaussianDiffusion:
 
             # Tạo bản sao của cfn để tối ưu
             cfn_optim = cfn.detach().clone().requires_grad_(True)
+            print('cfn_optim grad: ', cfn_optim.requires_grad)
             optimizer = th.optim.AdamW([cfn_optim], lr=0.001)
             lambda_eff = 0.1  # Hệ số cân bằng giữa việc giữ logits và phạt regularization
 
