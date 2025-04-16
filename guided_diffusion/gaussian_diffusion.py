0da2a42565279c1a8136b4ccf57461f038aa6ca5
Nguyen Thang Phuc <phuca1tt1bn@gmail.com>
2025-04-16 15:36:47 +0700

    saved

--- guided_diffusion/gaussian_diffusion.py
+++ guided_diffusion/gaussian_diffusion.py
@@ -503,6 +503,12 @@ class GaussianDiffusion:
             #cfn_reg = cfn_optim.detach().clone().requires_grad_(True)
             #cfn_logits = cfn_optim.detach().clone().requires_grad_(True)
 
+            # --- 1.MSE
+            # logits ban đầu (old_logits) tính từ mean ban đầu cộng với cfn ban đầu
+            mean_old = out["mean"] + out["variance"] * cfn
+            old_logits = classifier(mean_old, timesteps=t-1)
+            
+
             with th.enable_grad():
                 
                 for i in range(20):
@@ -532,12 +538,26 @@ class GaussianDiffusion:
                     #print(f"[Iter {i}] Grad của loss_logits: {grad_loss_logits}")
                     '''
 
-                    # --- Tính loss tổng trên đồ thị chính với cfn_optim ---
+                    
+                    # --- 1.MSE: Tính loss theo logits với hàm MSE ---
+                    # logits mới được tính từ mean có thêm cfn_optim
+                    mean_new = out["mean"] + out["variance"] * cfn_optim
+                    logits_new = classifier(mean_new, timesteps=t-1)
+
+                    loss_logits_main = th.nn.functional.mse_loss(logits_new, old_logits)
+                    
+                    '''
+                    # --- 1.BCE: Tính loss tổng trên đồ thị chính với cfn_optim ---
                     mean_new = out["mean"] + out["variance"] * cfn_optim
                     logits_new = classifier(mean_new, timesteps=t-1)
                     loss_logits_main = F.cross_entropy(logits_new, model_kwargs['y'], reduction="none").mean()
+                    '''
+
+                    # --- 2.Regularization: Tính loss Hiệu chỉnh
                     #loss_reg_main = th.mean(th.abs(cfn_optim - cfn_x0))
                     loss_reg_main = th.mean(th.abs(th.abs(cfn_optim) - cfn_x0[:, None, :, :])) ### 
+
+
                     loss = loss_logits_main + lambda_eff * loss_reg_main
                     loss.backward()
                     #print(f"[Iter {i}] Tổng Grad (sau backward): {cfn_optim.grad.detach()}")
