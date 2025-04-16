93010be9e644179fe7269f57af0302f0a96e1911
Nguyen Thang Phuc <phuca1tt1bn@gmail.com>
2025-04-17 05:10:39 +0700

    saved

--- guided_diffusion/gaussian_diffusion.py
+++ guided_diffusion/gaussian_diffusion.py
@@ -570,10 +570,10 @@ class GaussianDiffusion:
                     #loss_reg_main = th.mean(th.abs(cfn_optim - cfn_x0)) 
 
                     ### L1
-                    loss_reg_main = th.mean(th.abs(cfn_optim*(1-cfn_x0[:, None, :, :]))) #lambda_eff: 0.01
+                    #loss_reg_main = th.mean(th.abs(cfn_optim*(1-cfn_x0[:, None, :, :]))) #lambda_eff: 0.01
 
                     ### L2
-                    #loss_reg_main = th.nn.functional.mse_loss(cfn_optim * (1 - cfn_x0[:, None, :, :]), torch.zeros_like(cfn_optim))
+                    loss_reg_main = th.nn.functional.mse_loss(cfn_optim * (1 - cfn_x0[:, None, :, :]), torch.zeros_like(cfn_optim))
 
                     ### L2 giữa forward và backward --> đảm bảo sự thay đổi trong ảnh chỉ do những pixel tiềm năng thôi, còn lại nên bằng nhau
                     #loss_reg_main = th.nn.functional.mse_loss(mean_new, model_kwargs['noising'][int(t[0]-1)]) # có thể nhân thêm: (1 - cfn_x0[:, None, :, :]) cho từng cái, thì sẽ loại bỏ bớt những cái tiềm năng
