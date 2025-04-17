e2bf463f5bee81648a3489d7f9b9613c0e684c98
Nguyen Thang Phuc <phuca1tt1bn@gmail.com>
2025-04-17 15:42:06 +0700

    saved

--- guided_diffusion/gaussian_diffusion.py
+++ guided_diffusion/gaussian_diffusion.py
@@ -502,7 +502,7 @@ class GaussianDiffusion:
             cfn_optim = cfn.detach().clone().requires_grad_(True)
             #print('cfn_optim grad: ', cfn_optim.requires_grad)
             optimizer = th.optim.SGD([cfn_optim], lr=0.01, momentum=0.9, weight_decay=1e-4)
-            lambda_eff = 100  # Hệ số cân bằng giữa việc giữ logits và phạt regularization           
+            lambda_eff = 10000  # Hệ số cân bằng giữa việc giữ logits và phạt regularization           
             
             ###
             '''
