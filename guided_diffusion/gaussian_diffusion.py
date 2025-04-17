3679cf44efa1f938e078b292858828bd55004ca1
Nguyen Thang Phuc <phuca1tt1bn@gmail.com>
2025-04-17 22:18:19 +0700

    saved

--- guided_diffusion/gaussian_diffusion.py
+++ guided_diffusion/gaussian_diffusion.py
@@ -501,9 +501,10 @@ class GaussianDiffusion:
             # Tạo bản sao của cfn để tối ưu
             cfn_optim = cfn.detach().clone().requires_grad_(True)
             #print('cfn_optim grad: ', cfn_optim.requires_grad)
-            optimizer = th.optim.SGD([cfn_optim], lr=0.1, momentum=0.9, weight_decay=0)
-            lambda_eff = 10000  # Hệ số cân bằng giữa việc giữ logits và phạt regularization           
-            
+            optimizer = th.optim.SGD([cfn_optim], lr=0.1, momentum=0, weight_decay=0)
+            lambda_eff1 = 100
+            lambda_eff2 = 10000
+
             ###
             '''
             mean_old = out["mean"] + out["variance"] * cfn * 100
@@ -617,7 +618,7 @@ class GaussianDiffusion:
                     # ---------------------------------------------------------------------- #
                     
                     print(f'Time {int(t[0])} - loss_logits_main: {loss_logits_main} - loss_reg_main: {loss_reg_main}')
-                    loss = loss_logits_main + lambda_eff * loss_reg_main
+                    loss = lambda_eff1 * loss_logits_main + lambda_eff2 * loss_reg_main
                     loss.backward()
                     
                     #print(f"[Iter {i}] Tổng Grad (sau backward): {cfn_optim.grad.detach()}")
