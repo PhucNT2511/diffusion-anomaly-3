6254098aa4cc64f578818a56d621df31dd5d8238
Nguyen Thang Phuc <phuca1tt1bn@gmail.com>
2025-04-19 00:47:16 +0700

    saved

--- guided_diffusion/gaussian_diffusion.py
+++ guided_diffusion/gaussian_diffusion.py
@@ -632,6 +632,7 @@ class GaussianDiffusion:
                     #print(f'Time {int(t[0])} - loss_logits_main: {loss_logits_main.requires_grad} - loss_reg_main: {loss_reg_main.requires_grad}')
 
                     loss = loss_logits_main         #lambda_eff2 * loss_reg_main  + lambda_eff1 * loss_logits_main
+                    print(f'Time {int(t[0])} - loss: {loss}')
                     loss.backward()
                     
                     #print(f"[Iter {i}] Tổng Grad (sau backward): {cfn_optim.grad.detach()}")
