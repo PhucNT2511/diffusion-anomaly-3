da037896bcb61f9044c9612dc75fa754eb13a934
Nguyen Thang Phuc <phuca1tt1bn@gmail.com>
2025-04-18 16:46:49 +0700

    saved

--- guided_diffusion/gaussian_diffusion.py
+++ guided_diffusion/gaussian_diffusion.py
@@ -493,8 +493,8 @@ class GaussianDiffusion:
             out = p_mean_var.copy()
             cfn, a = cond_fn2(x, self._scale_timesteps(t).long(), **model_kwargs)
             #cfn_x0 = model_kwargs['grad_x0']
-            #cfn_x0 = 1 - model_kwargs["mask"]
-            cfn_x0 = model_kwargs["sample_mask"]
+            cfn_x0 = 1 - model_kwargs["mask"]
+            #cfn_x0 = model_kwargs["sample_mask"]
 
             plot_cfn_row(cfn) ### visualize
 
@@ -613,15 +613,13 @@ class GaussianDiffusion:
                     # Tránh chia cho 0 bằng cách cộng epsilon
                     eps_ = 1e-8
                     cfn_norm = cfn_optim / (cfn_max + eps_)         # [B, C, H, W]
+                    cfn_norm = cfn_norm*cfn_x0[:,None,:,:]
 
                     ### L1 
                     #loss_reg_main = th.mean(th.abs(cfn_norm))
 
-                    ### L1
-                    loss_reg_main = th.mean(th.abs(cfn_norm*cfn_x0)) #lambda_eff: 0.01
-
                     ### L2
-                    #loss_reg_main = th.nn.functional.mse_loss(cfn_norm , torch.zeros_like(cfn_norm))
+                    loss_reg_main = th.nn.functional.mse_loss(cfn_norm , torch.zeros_like(cfn_norm))
 
                     ### L2 giữa forward và backward --> đảm bảo sự thay đổi trong ảnh chỉ do những pixel tiềm năng thôi, còn lại nên bằng nhau
                     #loss_reg_main = th.nn.functional.mse_loss(mean_new, model_kwargs['noising'][int(t[0]-1)]) # có thể nhân thêm: (1 - cfn_x0[:, None, :, :]) cho từng cái, thì sẽ loại bỏ bớt những cái tiềm năng
