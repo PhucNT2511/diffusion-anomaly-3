780e5a7a7e528ae8c304ab563db7bb22cc50c1d5
Nguyen Thang Phuc <phuca1tt1bn@gmail.com>
2025-04-21 03:09:38 +0700

    saved

--- guided_diffusion/gaussian_diffusion.py
+++ guided_diffusion/gaussian_diffusion.py
@@ -504,7 +504,7 @@ class GaussianDiffusion:
             optimizer = th.optim.SGD([cfn_optim], lr=0.1, momentum=0, weight_decay=0) ########
             lambda_eff1 = 0.1
             lambda_eff2 = 5
-            lambda_eff3 = 10 # 100
+            lambda_eff3 = 5 # 100
 
             ###
             '''
