6b1141e6e034c7d884af5c8c4ee40ff7b8cba058
Nguyen Thang Phuc <phuca1tt1bn@gmail.com>
2025-05-20 07:05:57 +0700

    saved

--- guided_diffusion/gaussian_diffusion.py
+++ guided_diffusion/gaussian_diffusion.py
@@ -557,13 +557,13 @@ class GaussianDiffusion:
                     alpha_bar = _extract_into_tensor(self.alphas_cumprod, t, x.shape)
                     alpha_bar_prev = _extract_into_tensor(self.alphas_cumprod_prev, t, x.shape)
                     mean_pred = xstart_new * th.sqrt(alpha_bar_prev) + th.sqrt(1 - alpha_bar_prev) * eps_new
-                    '''                   
+                                      
                     logits_new    = classifier(mean_pred, self._scale_timesteps(t-1).long())
                     log_probs_new = F.log_softmax(logits_new, dim=-1)
                     selected = log_probs_new[range(len(log_probs_new)), model_kwargs['y'].view(-1)].sum()
-                    cfn_new = torch.autograd.grad(selected, mean_pred, create_graph=True)[0]
-                    '''
-                    cfn_new, _ = cond_fn2(mean_pred, self._scale_timesteps(t-1).long(), **model_kwargs)
+                    cfn_new = torch.autograd.grad(selected, mean_pred, create_graph=True)[0]                  
+                    #cfn_new, _ = cond_fn2(mean_pred, self._scale_timesteps(t-1).long(), **model_kwargs)
+
                     mse_loss_grad = th.sum(th.mean(th.abs(cfn_new - cfn_old), dim=(1,2,3))) ## sum() vì mean() sẽ bị scale, dù grad thì vẫn luôn độc lập giữa căc ảnh trong batch. Bởi lẽ, việc training inputs độc lập, ko phải training mạng shared giữa các input mà cần scale.
 
                     '''
@@ -621,7 +621,7 @@ class GaussianDiffusion:
                     print(f'Time {int(t[0])} - loss_logits_main: {loss_logits_main.requires_grad} - loss_reg_main: {loss_reg_main.requires_grad}')
                     #print(f'Time {int(t[0])} - bce_loss_1: {bce_loss_1} - bce_loss_2: {bce_loss_2} - loss_reg_main: {loss_reg_main}')
                     
-                    total_loss =  lambda_eff1 * loss_logits_main  + lambda_eff2 * loss_reg_main #+ lambda_eff3 * loss_reg_main_2
+                    total_loss =  lambda_eff2 * loss_reg_main #lambda_eff1 * loss_logits_main  + lambda_eff2 * loss_reg_main #+ lambda_eff3 * loss_reg_main_2
                     #print(f'Time {int(t[0])} - loss: {loss} - requires_grad: {loss.requires_grad}')
 
                     '''
