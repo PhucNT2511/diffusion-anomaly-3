9640d81b01d69e22f386b10ac5bad77a9d5b2522
Nguyen Thang Phuc <phuca1tt1bn@gmail.com>
2025-05-26 22:02:05 +0700

    saved

--- guided_diffusion/gaussian_diffusion.py
+++ guided_diffusion/gaussian_diffusion.py
@@ -514,14 +514,16 @@ class GaussianDiffusion:
 
             # visualize
             plot_cfn_row(cfn) 
+
+            cfn_1 = cfn * cfn_x0
             
             # Tạo bản sao của cfn để tối ưu
             cfn_optim = cfn.detach().clone().requires_grad_(True)
             #print('cfn_optim grad: ', cfn_optim.requires_grad)
-            optimizer = th.optim.SGD([cfn_optim], lr=100.0, momentum=0, weight_decay=0) ########
+            optimizer = th.optim.SGD([cfn_optim], lr=10.0, momentum=0, weight_decay=0) ########
             #optimizer = th.optim.Adam([cfn_optim], lr=0.01, betas=(0.9, 0.999), eps=1e-12) ########
             lambda_eff1 = 1  #
-            lambda_eff2 = 0.1 #
+            lambda_eff2 = 256*256 #
             lambda_eff3 = 100 #5
             '''
             eps_old = eps - (1 - alpha_bar).sqrt() * cfn * 100 #* cfn_x0
@@ -572,8 +574,8 @@ class GaussianDiffusion:
                     
                     # --- 1.BCE: Tính loss tổng trên đồ thị chính với cfn_optim thông qua mean_t --- Margin loss hoặc Dùng trực tiếp
                     #mean_new = out["mean"] + out["variance"] * cfn_optim * 100
-
-                    eps_new = eps - (1 - alpha_bar).sqrt() * cfn_optim * 100 * cfn_x0
+                    cfn_optim_1 = cfn_optim * cfn_x0
+                    eps_new = eps - (1 - alpha_bar).sqrt() * cfn_optim_1 * 100
                     xstart_new = self._predict_xstart_from_eps(x, t, eps_new)  ### Hai công thức này có vẻ tương đương nhau, chẳng qua chỉ là nhân chia --> bỏ qua một trong 2
                     alpha_bar = _extract_into_tensor(self.alphas_cumprod, t, x.shape)
                     alpha_bar_prev = _extract_into_tensor(self.alphas_cumprod_prev, t, x.shape)
@@ -626,7 +628,7 @@ class GaussianDiffusion:
                     ### L1 
                     #loss_reg_main = th.sum(th.mean(th.abs(cfn_optim), dim=(1, 2, 3)))
                     #loss_reg_main = th.mean(th.abs(mean_pred - x_deterministic))
-                    loss_reg_main = th.nn.functional.mse_loss(cfn_optim, cfn, reduction="mean")
+                    loss_reg_main = th.nn.functional.mse_loss(cfn_1, cfn_optim_1, reduction="mean")
 
 
                     #loss_reg_main_2 = th.mean(th.abs(cfn_optim * cfn_x0))
