7f48b8243cb098c055638876d4cbe505d08e3510
Nguyen Thang Phuc <phuca1tt1bn@gmail.com>
2025-05-27 00:21:00 +0700

    saved

--- guided_diffusion/gaussian_diffusion.py
+++ guided_diffusion/gaussian_diffusion.py
@@ -520,7 +520,7 @@ class GaussianDiffusion:
             # Tạo bản sao của cfn để tối ưu
             cfn_optim = cfn.detach().clone().requires_grad_(True)
             #print('cfn_optim grad: ', cfn_optim.requires_grad)
-            optimizer = th.optim.SGD([cfn_optim], lr=100.0, momentum=0, weight_decay=0) ########
+            optimizer = th.optim.SGD([cfn_optim], lr=1.0, momentum=0, weight_decay=0) ########
             #optimizer = th.optim.Adam([cfn_optim], lr=0.01, betas=(0.9, 0.999), eps=1e-12) ########
             lambda_eff1 = 1  #
             lambda_eff2 = 256*256 #
